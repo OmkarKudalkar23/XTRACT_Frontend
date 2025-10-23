131e680223fbf2199b95dd491e3d8ae09593bdcf
Jai Sumanth Nekkanti <jaisumanthnekkanti@gmail.com>
2025-10-24 01:20:43 +0530

    leaderboard design complete (text pending)

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Leaderboard from "./components/Leaderboard"
+function App() {
+  return (
+    <>
+      <Leaderboard></Leaderboard>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Leaderboard.jsx
@@ -0,0 +1,30 @@
+function Leaderboard(){
+    return(
+        <div className="w-full h-full overflow-hidden">
+            <div className="relative overflow-hidden min-w-screen h-screen">
+                <div>
+                    <img src="\assets\images\unnamed 30.jpg" className="min-w-screen h-screen absolute"></img>
+                </div>
+                <div>
+                    <img src="\assets\images\blood.png" className="absolute w-[20vw] h-[30vh] left-[66%] top-[2%]"></img>
+                </div>
+                <div>
+                    <img src="\assets\images\spiderweb.png" className="absolute w-[16vw] h-[18vh] opacity-70 left-[20%] top-[6%]"></img>
+                </div>
+                <div>
+                    <img src="\assets\images\props.png" className="absolute w-[25vw] h-[33vh] left-[3%] top-[60%]"></img>
+                </div>
+                <div>
+                    <img src="\assets\images\skull.png" className="absolute w-[20vw] h-[40vh] left-[80%] top-[60%]"></img>
+                </div>
+                <div>
+                    <img src="\assets\images\prop2.png" className="absolute h-[60vh] w-[5vw] left-[1%]"></img>
+                </div>
+                <div>
+                    <img src="\assets\images\prop3.png" className="absolute h-[60vh] w-[8vw] left-[90%]"></img>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Leaderboard
